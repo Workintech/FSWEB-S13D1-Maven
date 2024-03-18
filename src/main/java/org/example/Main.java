9728537a41f9488a7a7232bab5cc086d0db49fb7
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-03-18 15:53:43 +0300

    test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,24 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (!isBarking || clock < 0 || clock > 23) {
+            return false;
+        } else if (clock < 8 || clock >= 20) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upperLimit = (isSummer) ? 45 : 35;
+        return (temp >= 25 && temp <= upperLimit);
     }
 
     public static double area(double width, double height) {
