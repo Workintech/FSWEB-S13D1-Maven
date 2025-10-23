2465d65e07a9e1ef6531b44beeb2d4dfd2d2ddcf
rookiecoder34 <ayaydintolga@gmail.com>
2025-10-23 16:18:36 +0300

    Havlayan Kopegimiz ve Yas Tespiti okey.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -7,39 +9,59 @@ public class Main {
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
 
-        if (clock<0 || clock>23) {
+        if (clock < 0 || clock > 23) {
             return false;
         }
-        if (isBarking==false) {
+        if (isBarking == false) {
             return false;
         }
-        if (clock < 8 || clock > 20 ) {
+        if (clock < 8 || clock > 20) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       if ((firstAge>13 && firstAge<19)||(secondAge>13 && secondAge<19)||(thirdAge>13 && thirdAge<19)) {
-           return true;
-       }
-       else {
-           return false;
-       }
+        if ((firstAge > 13 && firstAge < 19) || (secondAge > 13 && secondAge < 19) || (thirdAge > 13 && thirdAge < 19)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (!isSummer) {
+            if (temp >= 25 && temp <= 35) {
+                return true;
+            }
+        }
+        else if (isSummer) {
+            if (temp >= 35 && temp <= 45) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static double area(double width, double height) {
-       
+        if  (width < 0 || height < 0) {
+            return -1;
+        }
+        else {
+            return width * height;
+        }
     }
 
     public static double area(double radius) {
-       
+        if(radius < 0) {
+            return -1;
+        }
+        else {
+            return Math.pow(radius, 2)*Math.PI;
+        }
     }
 }
+
