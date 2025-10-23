7a4ca79f58244a3f9ec2abaf01870f3948cda32c
rookiecoder34 <ayaydintolga@gmail.com>
2025-10-23 14:16:37 +0300

    Havlayan Kopegimiz ve Yas Tespiti okey.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,29 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        if (clock<0 || clock>23) {
+            return false;
+        }
+        if (isBarking==false) {
+            return false;
+        }
+        if (clock < 8 || clock > 20 ) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if ((firstAge>13 && firstAge<19)||(secondAge>13 && secondAge<19)||(thirdAge>13 && thirdAge<19)) {
+           return true;
+       }
+       else {
+           return false;
+       }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
